c6774c0f0b82380825d9ff3d9b6a2100f173105c
Vladimir Danilov <voffka.danilov@gmail.com>
2022-08-31 02:04:31 +0300

    Created HelloUser

---
+++ Lesson1_Examples/example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа, где пользователя просят ввести свое имя и приветствуют его.
+
+Console.WriteLine("Введите свое имя: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
