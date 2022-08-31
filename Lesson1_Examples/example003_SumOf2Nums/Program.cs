5b9ba107a1633158b000d2ea4e65dfb4a079d2e7
Vladimir Danilov <voffka.danilov@gmail.com>
2022-08-31 13:46:38 +0300

    One more try tu push

---
+++ Lesson1_Examples/example003_SumOf2Nums/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа нахождения суммы двух чисел
+
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(first + second);
