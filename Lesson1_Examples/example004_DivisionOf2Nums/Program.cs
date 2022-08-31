ab953a019da9be1df5a5ea6862a19c3aeddf4152
Vladimir Danilov <voffka.danilov@gmail.com>
2022-08-31 14:34:26 +0300

    Added DivisionOf2Nums

---
+++ Lesson1_Examples/example004_DivisionOf2Nums/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа нахождения частного двух чисел
+
+Console.WriteLine("Введите первое число: ");
+double first = Convert.ToInt32(Console.ReadLine()); // double вместо int, чтобы получать дроби
+Console.WriteLine("Введите второе число: ");
+double second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(first / second);
