fa0c9b52290763403bdc761ff37588fc22e74eb1
Vladimir Danilov <voffka.danilov@gmail.com>
2022-08-31 20:15:59 +0300

    Added FindMax

---
+++ Lesson1_Examples/example007_FindMax/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти максимальное число
+
+int a = 0;
+int b = 12;
+int c = 25;
+int d = 11;
+int e = 10;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
