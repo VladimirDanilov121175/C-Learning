8d0af578094c9b6a62ca326a7b823d9c904919ad
Vladimir Danilov <voffka.danilov@gmail.com>
2022-08-31 15:17:41 +0300

    Added Generator

---
+++ Lesson1_Examples/example005_Generator/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа нахождения суммы двух случайных чисел
+// через оператор new Random().Next();
+// Next задает диапазон, в котором будут генерироваться числа, при этом
+// последнее число диапазона выдает число всегда меньше на 1, т.е.
+// (min, max-1)
+
+int first = new Random().Next(1, 101);
+Console.WriteLine(first);
+int second = new Random().Next(1, 101);
+Console.WriteLine(second);
+Console.WriteLine(first + second);
