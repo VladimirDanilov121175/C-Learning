da0c7fc570f1ccd5e1dd2fb8e35b56587c3377e5
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-01 00:42:02 +0300

    Added code for finding a sqare of any number

---
+++ Seminar1_OnlineWork/task001_Square/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа для нахождения квадрата введенного числа
+
+Console.Write("Введите число, чтобы узнать его квадрат: ");
+double num = Convert.ToDouble(Console.ReadLine()); // double, чтобы можно было вводить дроби
+double square = num * num;
+Console.WriteLine(square); 
