be8b84abd0c346afa2a5b32cccc5c087ad1860fb
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-01 20:07:43 +0300

    Added program FindMax

---
+++ Seminar1_HomeWork/task002_FindMax/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+// Приглашение пользователю ввести два числа
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+// Сравниваем и выводим введенные числа
+if (first > second)
+    Console.Write("Максимальное число: " + first);
+else if (first == second)
+    Console.Write("Числа равны");
+else
+    Console.Write("Максимальное число: " + second);
