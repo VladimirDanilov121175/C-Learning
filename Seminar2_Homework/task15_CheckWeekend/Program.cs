ba4d26e4e2aa90bd11dad757db92769381f0c93e
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-04 00:45:40 +0300

    Homework for Seminar 2 added

---
+++ Seminar2_Homework/task15_CheckWeekend/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да 
+7 -> да 
+1 -> нет
+*/
+Console.Clear();
+
+void CheckWeekend(int number)
+{
+    if (number > 5)
+        Console.WriteLine("Weekend!");
+    else
+        Console.WriteLine("Working day :(");
+}
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+while (day < 1 || day > 7)
+{
+    Console.Write("Цифры должны быть от 1 до 7! Повторите ввод: ");
+    day = Convert.ToInt32(Console.ReadLine());
+}
+CheckWeekend(day);
