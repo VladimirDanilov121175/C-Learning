bb23378742d3f334ed7eeffec81cf38ef05ad0f3
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-06 22:25:57 +0300

    Tasks from Seminar 3 added

---
+++ Seminar3_OnlineWork/task017_WhichQuarter/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0, и выдаёт номер четверти плоскости, в которой находится эта точка. */
+
+// Метод страховки от введения нуля
+int CheckForZeroInput(int input)
+{
+    while (input == 0)
+    {
+        Console.Write("Число не должно быть 0! Повторите ввод: ");
+        input = Convert.ToInt32(Console.ReadLine());
+    }
+    return input;
+}
+
+Console.WriteLine("Введите x, отличный от 0: ");
+int x = CheckForZeroInput(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Введите y, отличный от 0: ");
+int y = CheckForZeroInput(Convert.ToInt32(Console.ReadLine()));
+
+Console.Write($"Введена координатная точка ({x}, {y}). Это ");
+
+if (x > 0)
+{
+    if (y > 0)
+        Console.WriteLine("первая четверть.");
+    else
+        Console.WriteLine("четвертая четверть.");
+}
+else
+if (y > 0)
+    Console.WriteLine("вторая четверть.");
+else
+    Console.WriteLine("третья четверть.");
---
+++ Seminar3_OnlineWork/task021_DistanceXYin2D/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21 */
+
+Console.WriteLine("Введите последовательно координаты x1, y1, x2, y2: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine(distance);
