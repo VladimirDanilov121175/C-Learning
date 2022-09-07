870bb22ef95b548e064c3f863c0838eb49c8971e
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-08 00:17:26 +0300

    Added Homework for Seminar 3

---
+++ Seminar3_Homework/Task019_Polindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом. */
+
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// Первый способ - математический
+int fig1 = n / 10000, fig2 = n / 1000 % 10,
+    fig4 = n % 100 / 10, fig5 = n % 10;
+Console.WriteLine(fig1 + " " + fig2 + " " + fig4 + " " + fig5);
+
+if (fig1 == fig5 && fig2 == fig4)
+    Console.WriteLine("Полиндром");
+else
+    Console.WriteLine("Не полиндром");
+
+// Второй способ - программистский
+if (Convert.ToString(n)[0] == Convert.ToString(n)[4] &&
+    Convert.ToString(n)[1] == Convert.ToString(n)[3])
+    Console.WriteLine("Полиндром");
+else
+    Console.WriteLine("Не полиндром");
---
+++ Seminar3_Homework/task023_CubedSheet/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N. 
+3 -> 1, 8, 27 5 -> 1, 8, 27, 64, 125 */
+Console.Clear();
+
+Console.Write("Таблица кубов чисел от 1 до (введите число): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    while (i < n)
+    {
+        Console.Write(", ");
+        break;
+    }
+}
