8789a09337d6a982942d5c37d2ae5dca7016d86c
Vladimir Danilov <voffka.danilov@gmail.com>
2022-09-22 19:10:23 +0300

    Added homework for seminar 7

---
+++ Seminar7_Homework/task047_RandDoubleMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+Console.Clear();
+
+double[,] CreateMatrix()
+{
+    Console.WriteLine("Введите последовательно размер двумерного массива");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    double[,] matrix = new double[x, y];
+    Console.WriteLine($"Задан двумерный массив размером [{x}, {y}]");
+    Console.WriteLine();
+
+    return matrix;
+}
+
+void FillMatrixWithRealNumbers(double[,] matrix)
+{
+    int randomizer;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            randomizer = new Random().Next(0, 3);
+            
+            matrix[i, j] = Math.Round(new Random().Next(-10, 11)
+                         + new Random().NextDouble(), randomizer);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+double[,] newMatrix = CreateMatrix();
+FillMatrixWithRealNumbers(newMatrix);
+PrintMatrix(newMatrix);
---
+++ Seminar7_Homework/task050_ReturnNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+ */
+Console.Clear();
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+void ShowThisElement(int[,] matrix)
+{
+    Console.Write("Введите номер строки: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите номер столбца: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    if (x > matrix.GetLength(0) - 1 || y > matrix.GetLength(1) - 1)
+        Console.WriteLine("Такого элемента в массиве нет");
+    else
+        Console.WriteLine($"Под индексом [{x}, {y}] хранится значение {matrix[x, y]}");
+}
+
+Console.WriteLine("Задан двумерный массив");
+int[,] newMatrix =  {
+                    {1, 2, 3, 4, 5},
+                    {10, 11, 12, 13, 14},
+                    {21, 22, 23, 24, 25},
+                    {101, 102, 103, 104, 105}
+                    };
+PrintMatrix(newMatrix);
+ShowThisElement(newMatrix);
